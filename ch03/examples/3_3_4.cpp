d3f8657283c127593e8c4ad2ce86aecaf87c5131
j-thach000 <jamesthach64@gmail.com>
2022-09-24 00:06:37 -0400

    Update file names

--- ch03/examples/3_3_4.cpp
+++ ch03/examples/3_3_4.cpp
@@ -1,13 +1,13 @@
 #include "std_lib_facilities.h"
 
-// 
+// try this
 int main()
 {
     std::cout << "Please enter your first name and age\n";
     std::string first_name = "???";
     double age = 0;                     
     std::cin >> first_name >> age;    
-    std::cout << "Age in months: " << age * 12;          
-    std::cout << "Hello, " << first_name << " (age " << age << ")\n";
+    std::cout << "Age in months: " << age * 12 << '\n';          
+    std::cout << "Hello, " << first_name << " (age in years: " << age << ")\n";
     keep_window_open();
 }
