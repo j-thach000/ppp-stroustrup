055339ca655208d37b472ff005a29743fea61528
James Thach <jthach000@Jamess-MBP.attlocal.net>
2022-10-07 11:45:51 -0400

    Add ch04 if-else example

--- ch04/examples/4_4_1_1.cpp
+++ ch04/examples/4_4_1_1.cpp
@@ -1,6 +1,38 @@
 #include "std_lib_facilities.h"
 
+/*
+selection statements (instructions that determine which statements execute)
+if-else
+switch
+*/
 int main()
 {
-    
+        // if acts as the simplest selection tool
+    int a = 0;
+    int b = 0;
+    std::cin >> a >> b; // can enter both inputs on the same line
+
+    if (a < b) 
+        std::cout << "max(" << a << ", " << b << ") is " << b << '\n';
+    else       
+        std::cout << "max(" << a << ", " << b << ") is " << a << '\n';
+
+    std::cout << '\n';
+
+    // convert from inches to centimeters or centimeters to inches
+    // a suffix "i" or "c" indicates the unit of the input
+    // works as intended on windows when compiled and ran but not macos
+
+    constexpr double cm_per_inch = 2.54;
+
+    double length = 1; // length to be converted
+    char unit = 0;     // unit to be converted, i or c
+
+    std::cout << "Please enter a length followed by a unit (c or i):\n";
+    std::cin >> length >> unit;
+
+    // we never test for the case of centimeters specifically
+    // it should do exactly what we intended and spit out and error otherwise
+    if (unit == 'i') std::cout << length << "in == " << length * cm_per_inch << "cm\n";
+    else             std::cout << length << "cm == " << length / cm_per_inch << "in\n";
 }
