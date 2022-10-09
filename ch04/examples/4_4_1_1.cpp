3be80eee39a93ce8e23191fa59943f11fe7defad
James Thach <jthach000@Jamess-MBP.attlocal.net>
2022-10-09 17:26:04 -0400

    Update example to account for invalid user input

--- ch04/examples/4_4_1_1.cpp
+++ ch04/examples/4_4_1_1.cpp
@@ -22,21 +22,22 @@ int main()
     // convert from inches to centimeters or centimeters to inches
     // a suffix "i" or "c" indicates the unit of the input
 
+    std::cout << "[length converter v2]\n";
+
     constexpr double cm_per_inch = 2.54;
+    
+    double length = 1;
+    char unit = 0;
 
-    double length = 1; // length to be converted
-    char unit = 0;     // unit to be converted, i or c
-
-    std::cout << "Please enter a length followed by a unit (c or i):\n";
+    std::cout << "Enter a length and type of unit to be converted: ";
     std::cin >> length >> unit;
 
-    // we never test for the case of centimeters specifically
-    // it should do exactly what we intended and spit out and error otherwise
-    if (unit == 'i') std::cout << length << "in == " << length * cm_per_inch << "cm\n";
-    else             std::cout << length << "cm == " << length / cm_per_inch << "in\n";
+    // account for potential incorrect input from users
+    if      (unit == 'i')
+         std::cout << length << "in == " << length * cm_per_inch << "cm\n";
+    else if (unit == 'c')
+         std::cout << length << "cm == " << length / cm_per_inch << "in\n";
+    else 
+         std::cout << "Sorry," << unit << " is an invalid unit of measurement for this program.\n";
 
-    /*
-    the program works but not exactly with the same input as macos
-    something about the clang compiler thinking any xi input being an imaginary number (x being an integer)
-    */
 }
