303576701dc6be666c74df36eeb842b99c0fbd11
j-thach000 <jamesthach64@gmail.com>
2022-11-05 01:17:54 -0400

    Add completed exercise

--- ch04/exercises/4_5.cpp
+++ ch04/exercises/4_5.cpp
@@ -7,3 +7,41 @@ represent an operation. If the entry arguments are 35.6, 24.1, and '+', the
 program output should be The sum of 35.6 and 24.1 is 59.7. In Chapter 6
 we look at a much more sophisticated simple calculator.
 */
+
+#include <iostream>
+
+int main()
+{
+    double a = 0;
+    std::cout << "Please input a value    : ";
+    std::cin >> a;
+    double b = 0;
+    std::cout << "Please input a value    : ";
+    std::cin >> b;
+    char operation;
+    std::cout << "Please input a character: ";
+    std::cin >> operation;
+    
+    double result = 0;
+
+    switch (operation)
+    {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break; 
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;      
+        default:
+            std::cout << "Unknown operation. Please enter an arithmetic operation: + - * /";
+            break; 
+    }
+
+    std::cout << "Result                  : " << result << '\n';
+}
